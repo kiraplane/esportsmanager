532616e40be69072cc1dfa75252d0fe2efe5c19b
kiraplane <zhh0504@gmail.com>
2026-08-03 12:38:43 +0800

    chore: refresh site growth data for 2026-08-03

--- src/data/esportsmanager/guides.ts
+++ src/data/esportsmanager/guides.ts
@@ -1,6 +1,6 @@
 import type { Guide, GuideVideo } from './types';
 
-const checkedAt = '2026-08-01';
+const checkedAt = '2026-08-03';
 
 function youtubeThumbnail(id: string) {
   return `https://i.ytimg.com/vi/${id}/hq720.jpg`;
