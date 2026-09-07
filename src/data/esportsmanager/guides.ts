cf9cb1b48c598b19f7a3c83b3139e302e91a099a
kiraplane <zhh0504@gmail.com>
2026-09-07 10:20:28 +0800

    chore: refresh game guide content for 2026-09-07

--- src/data/esportsmanager/guides.ts
+++ src/data/esportsmanager/guides.ts
@@ -1,6 +1,6 @@
 import type { Guide, GuideVideo } from './types';
 
-const checkedAt = '2026-08-24';
+const checkedAt = '2026-09-07';
 
 function youtubeThumbnail(id: string) {
   return `https://i.ytimg.com/vi/${id}/hq720.jpg`;
